116ef39b3ed6241c60d6c035a6560d83ffe52e82
Amey-Kalpe <66259269+Amey-Kalpe@users.noreply.github.com>
2024-10-04 08:08:14 +0000

    added baseline regressor and metric_threshold for model evaluation

--- custom_model_with_eval/main.py
+++ custom_model_with_eval/main.py
@@ -18,14 +18,16 @@ from pathlib import Path
 from predictor import predictor
 import joblib
 from mlflow.models import make_metric
+from sklearn.dummy import DummyRegressor
+from mlflow.models import MetricThreshold
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
 # get arguments from command
 parser = argparse.ArgumentParser()
-parser.add_argument("--alpha", type=float, required=False, default=0.4)
-parser.add_argument("--l1_ratio", type=float, required=False, default=0.4)
+parser.add_argument("--alpha", type=float, required=False, default=0.05)
+parser.add_argument("--l1_ratio", type=float, required=False, default=0.1)
 args = parser.parse_args()
 
 
@@ -35,6 +37,21 @@ def eval_metrics(actual, pred):
     mae = mean_absolute_error(actual, pred)
     r2 = r2_score(actual, pred)
     return rmse, mae, r2
+
+
+def train_dummy_regressor(train_x, train_y, **kwargs):
+    if "alpha" in kwargs and "l1_ratio" in kwargs:
+        baseline_regressor = DummyRegressor(
+            alpha=kwargs["alpha"], l1_ratio=kwargs["l1_ratio"]
+        )
+    else:
+        baseline_regressor = DummyRegressor()
+    baseline_regressor.fit(train_x, train_y)
+    return baseline_regressor
+
+
+def dump_model(model, model_path):
+    joblib.dump(model, model_path)
 
 
 if __name__ == "__main__":
@@ -91,38 +108,67 @@ if __name__ == "__main__":
 
         (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
+        baseline_regressor = train_dummy_regressor(train_x, train_y)
+        baseline_predicted_qualities = baseline_regressor.predict(test_x)
+
         print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
         print("  RMSE: %s" % rmse)
         print("  MAE: %s" % mae)
         print("  R2: %s" % r2)
 
         sklearn_model_path = "sklearn_model.pkl"
-        joblib.dump(lr, sklearn_model_path)
+        baseline_model_path = "baseline_sklearn_regressor.pkl"
+        dump_model(lr, sklearn_model_path)
+        dump_model(baseline_regressor, baseline_model_path)
 
         data_dir = "red-wine-data"
         train_x.to_csv(f"{data_dir}/train_data.csv")
         test_x.to_csv(f"{data_dir}/test_data.csv")
 
-        artifacts = {"sklearn_model": sklearn_model_path, "data": data_dir}
+        sklearn_artifacts = {"sklearn_model": sklearn_model_path, "data": data_dir}
+        baseline_artifacts = {
+            "baseline_sklearn_regressor": baseline_model_path,
+            "data": data_dir,
+        }
 
         class SklearnModel(mlflow.pyfunc.PythonModel):
+            def __init__(self, artifact_name):
+                self.artifact_name = artifact_name
+
             def load_context(self, context):
-                self.sklearn_model = joblib.load(context.artifacts["sklearn_model"])
+                self.sklearn_model = joblib.load(context.artifacts[self.artifact_name])
 
             def predict(self, context, model_input):
                 return self.sklearn_model.predict(model_input.values)
 
+        thresholds = {
+            "mean_squared_error": MetricThreshold(
+                threshold=0.5,
+                min_absolute_change=0.05,
+                min_relative_change=0.05,
+                greater_is_better=False,
+            )
+        }
+
         mlflow.pyfunc.log_model(
             artifact_path="custom_sklearn_artifacts",
-            python_model=SklearnModel(),
-            artifacts=artifacts,
+            python_model=SklearnModel(sklearn_model_path.split(".")[0]),
+            artifacts=sklearn_artifacts,
+            code_paths=["main.py"],
+            pip_requirements="../requirements.txt",
+        )
+
+        mlflow.pyfunc.log_model(
+            artifact_path="baseline_sklearn_regressor_artifacts",
+            python_model=SklearnModel(baseline_model_path.split(".")[0]),
+            artifacts=baseline_artifacts,
             code_paths=["main.py"],
             pip_requirements="../requirements.txt",
         )
 
         # Create Custom Metrics
-        # While creating custom metrics, use _builtin_metrics if dealing with the mlflow builtin metrics and
-        # use builtin_metrics otherwise, same goes with eval_df
+        # While creating custom metrics, use _builtin_metrics if not dealing with the mlflow builtin metrics and
+        # use builtin_metrics otherwise, same goes with eval_df, see below
         def square_diff_plus_one(eval_df, _builtin_metrics):
             return np.sum(np.abs((eval_df["prediction"] - eval_df["target"] + 1) ** 2))
 
@@ -141,19 +187,24 @@ if __name__ == "__main__":
             name="sum_on_target_by_two",
         )
 
-        artifacts_uri = mlflow.get_artifact_uri("custom_sklearn_artifacts")
-        print(f"ARTIFACTS URI: {artifacts_uri}")
+        sklearn_artifacts_uri = mlflow.get_artifact_uri("custom_sklearn_artifacts")
+        baseline_artifacts_uri = mlflow.get_artifact_uri(
+            "baseline_sklearn_regressor_artifacts"
+        )
 
         # Evaluate the model
         mlflow.evaluate(
-            model=artifacts_uri,
+            model=sklearn_artifacts_uri,
             data=test,
             model_type="regressor",
             targets="quality",
             evaluators=["default"],
             custom_metrics=[metric_squared_diff_plus_one, metric_sum_on_target_by_two],
+            validation_thresholds=thresholds,
+            baseline_model=baseline_artifacts_uri,
         )
 
+        # Load the custom model
         # custom_model = mlflow.pyfunc.load_model(
         #     model_uri="runs:/76354552393f44d28faef66fed93a612/custom_sklearn_artifacts"
         # )
